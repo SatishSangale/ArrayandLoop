d3281981a563e0e4d791581e5e6e27509b695a76
SatishSangale <satishsangale0702@gmail.com>
2024-08-22 21:41:12 +0530

    Add files via upload

---
+++ printAllelementsArray.java
@@ -0,0 +1,28 @@
+package ArrayPractice;
+
+public class printAllelementsArray {
+    public static void main(String[] args) {
+        int[] arr = {20,55,66,77,25,56,89,99};
+        for(int i = 0;i < arr.length;i++){
+            //i = 0
+            //lets execute code here
+            System.out.println(arr[i]); //
+
+        }
+
+        // i have 50 students, i want to start iteration from 10th student till 30th student
+        //for(int i = 9;i < 30; i++){}
+        //for(int i = 49; i <= 29; i--){}
+
+       /* System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        System.out.println(arr[5]);
+        System.out.println(arr[6]);
+        System.out.println(arr[7]);
+
+        */
+    }
+}

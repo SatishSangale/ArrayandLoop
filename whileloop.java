800be04d3046e73054edb05d9442d7b1890ac08b
SatishSangale <satishsangale0702@gmail.com>
2024-08-22 21:42:52 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,28 @@
+package ArrayPractice;
+
+public class whileloop {
+    public static void main(String [] args){
+        //initialization
+        /* int i = 1;
+        //test
+        while (i < 5) {
+            System.out.println(" Hello world");
+            //update
+            i++;
+            }*/
+
+        /*int aa = 2, sum = 0;
+        while (aa <= 10 ) {
+            sum = sum + aa;
+            aa++;
+            System.out.println("Summation: " + sum);
+        }
+         */
+        int i = 0;
+        while ( i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}

a68ec85cff79d6c996a8b89b3694854040ca2f5d
SatishSangale <satishsangale0702@gmail.com>
2024-08-22 21:43:14 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,53 @@
+package ArrayPractice;
+
+public class dowhileloop {
+    public static void main(String[] args) {
+        //initial counter variable
+        /*int i = 0 ;
+        do {
+            //body of loop that will execute minimum
+            //1 time for sure no matter what
+            System.out.println("print statement");
+            i++;
+        }
+        //checking conditon
+        //note: it is being checked after
+        //minimum 1 iteration
+        while( i <= 5 );
+
+    }
+         */
+       /* int i = 0;
+        do{
+            System.out.println(" do while loop practice");
+            i++;
+        }
+        while(i < 0);
+    }
+    */
+      /*  int i = 1;
+        do {
+            System.out.println(" Hello world");
+            i++;
+        }
+        while(i< 7);
+    }
+     */
+
+      /* int x = 21, sum = 0;
+       do{
+           sum += x;
+           x--;
+       } while (x > 10);
+       System.out.println("Summation:"+ sum);
+    }
+
+       */
+
+        int i = 1;
+        do {
+            System.out.println(" do while loop program in java");
+        }
+        while (i >= 3);
+    }
+}

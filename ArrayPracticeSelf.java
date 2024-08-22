00159a339030f9dd931b12e148340604d62b53d3
SatishSangale <satishsangale0702@gmail.com>
2024-08-22 21:42:29 +0530

    Add files via upload

---
+++ ArrayPracticeSelf.java
@@ -0,0 +1,37 @@
+package ArrayPractice;
+
+public class ArrayPracticeSelf {
+    public static void main(String [] args) {
+        //1 Syntax for creating array
+        //datatype[] refenceNAme = new datatype[size];
+        // 0 9
+  /*  int [] arrrr = new int[10];// only create array not stored element
+    arrrr[0] = 22;
+    arrrr[4] = 50;
+    arrrr[1] = 20;
+    arrrr[2] = 10;
+    arrrr[3] = 30;
+    arrrr[5] = 40;
+    arrrr[6] = 60;
+    arrrr[8] = 70;
+    arrrr[9] = 90;
+    arrrr[7] = 110;
+*/
+       /* int [] students = new int[5];
+        students[0] = 7;
+        students[2] = 23;
+        students[3] =21;
+        */
+       /* int [] number = new int[2];
+        number[1] = 12;
+        number[0] = 7;
+        */
+
+        //2 syntax for creating array
+        //datatypes[] referenceName = {value2, value2, value3};
+        int[] arrayName = {10,20,30,40,50,60,90,245843,555556};
+        int[] study = {1,2,3,4};
+        int[] lecture = {212,368,874168,};
+        String[] classname ={"first","second"};
+    }
+}
